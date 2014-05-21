20f1aee12020b230335b116322ae9ea875e8b5cb
Scott Erickson <sderickson@gmail.com>
2014-05-21 10:49:23 -0700

    Updated and did some filling out for the testing framework, making tests for my most recent changes.

--- tests/specs/backbone-mediator_test.js
+++ tests/specs/backbone-mediator_test.js
@@ -1,24 +1,39 @@
-define([
-  'underscore',
-  'backbone',
-  'backbone-mediator'
-], function(_, Backbone){
-  'use strict';
-
-  var spies = {};
-
-  describe("Backbone-Mediator", function () {
-
-    beforeEach(function(){
-      $('body').append('<div id="container"><a></a></div>');
-    });
-
-    afterEach(function(){
-      _.each(spies, function(spy){
-        spy.restore();
-      });
-    });
-
+describe("Validation system", function () {
+  
+  beforeEach(function() {
+    Backbone.Mediator.init();
+  });
+  
+  it("emits a warning when you publish on an undefined channel", function() {
+    spyOn(console, 'warn');
+    Backbone.Mediator.subscribe('1', function(e) {});
+    expect(console.warn).toHaveBeenCalled();
+  });
+  
+  it("blocks publications that do not follow a defined schema", function() {
+    obj = {};
+    obj.func = function () {};
+    spyOn(obj, 'func');
+    Backbone.Mediator.subscribe('1', obj.func);
+    Backbone.Mediator.addChannelSchema('1', { type: 'string' });
+    Backbone.Mediator.publish('1', 5);
+    expect(obj.func).not.toHaveBeenCalled();
   });
 
+  it("emits an error when a publication has been blocked", function() {
+    spyOn(console, 'error');
+    Backbone.Mediator.addChannelSchema('1', { type: 'string' });
+    Backbone.Mediator.publish('1', 5);
+    expect(console.error).toHaveBeenCalled();
+  });
+  
+  it("allows publications which validate with the schema", function() {
+    obj = {};
+    obj.func = function () {};
+    spyOn(obj, 'func');
+    Backbone.Mediator.subscribe('1', obj.func);
+    Backbone.Mediator.addChannelSchema('1', { type: 'string' });
+    Backbone.Mediator.publish('1', 'string');
+    expect(obj.func).toHaveBeenCalled();
+  });
 });
